d96a0123cb6c3023f854c9deef9e6d06476b8595
Julien Bohnsack <116066759+JulienRik@users.noreply.github.com>
2025-03-28 16:40:45 +0100

    Delete .config/fish directory

--- .config/fish/conf.d/z.fish
+++
@@ -1,6 +0,0 @@
-
-if test -f /usr/local/share/z.lua/z.lua
-    lua /usr/local/share/z.lua/z.lua --init fish enhanced once fzf | source
-else
-    lua /opt/homebrew/opt/z.lua/share/z.lua/z.lua --init fish enhanced once fzf | source
-end
